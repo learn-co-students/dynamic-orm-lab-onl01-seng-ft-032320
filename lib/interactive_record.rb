525e6ff622af798e977d171db6e9a82f9d498d6c
rwparrish <rino3717@gmail.com>
2020-04-25 11:54:14 -0600

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,11 +52,8 @@ class InteractiveRecord
         column_name = attribute.keys[0].to_s
         value_name = attribute.values[0]
     
-        sql = <<-SQL
-          SELECT * FROM #{table_name}
-          WHERE #{column_name} = ?
-          SQL
+        sql = "SELECT * FROM #{table_name} WHERE #{column_name} = ?"
     
-        DB[:conn].execute(sql, value_name);
-      end
+        DB[:conn].execute(sql, value_name)
+    end
 end
